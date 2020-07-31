b1ab82441441cd6ecf3fe266f2d907351021f9da
MrFabkirox <fabrice.esope@gmail.com>
2020-07-31 08:54:15 +0800

    test still ko

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("./SimpleSmartContract.sol");
+const SimpleSmartContract = artifacts.require("SimpleSmartContract");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(SimpleSmartContract);
 };
